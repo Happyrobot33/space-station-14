9bb05c125669e12620ff96f30b22e17394dcbe9e
Matthew Herber <matthewherber@sbcglobal.net>
2025-10-14 12:31:36 -0400

    tunnel vision rotation
    getting the position right is gonna be a PITA

--- Content.Client/_Starlight/Overlay/Overlays/TunnelVisionOverlay.cs
+++ Content.Client/_Starlight/Overlay/Overlays/TunnelVisionOverlay.cs
@@ -1,8 +1,32 @@
+using System.Numerics;
 using Robust.Client.Graphics;
+using Robust.Client.Input;
 
 namespace Content.Client._Starlight.Overlay;
 
 public sealed class TunnelVisionOverlay : BaseVisionOverlay
 {
+    [Dependency] private readonly IInputManager _inputManager = default!;
+    [Dependency] private readonly SharedEyeSystem _sharedEyeSystem = default!;
+    
     public TunnelVisionOverlay(ShaderPrototype shader) : base(shader) { ZIndex = (int?)OverlayZIndexes.TunnelVision; }
+
+    //add logic to beforedraw
+    protected override bool BeforeDraw(in OverlayDrawArgs args)
+    {
+        base.BeforeDraw(args);
+
+        //get angle between player and mouse
+        var mousePos = _inputManager.MouseScreenPosition.Position;
+
+        //get rotation relative to 0.5, 0.5
+        var screenCenter = new Vector2(args.Viewport.Size.X / 2, args.Viewport.Size.Y / 2);
+        var dir = (mousePos - screenCenter).Normalized();
+        var angle = MathF.Atan2(dir.Y, dir.X);
+
+        //pass the angle as radians to the shader
+        _shader.SetParameter("angle", angle);
+
+        return true;
+    }
 }
